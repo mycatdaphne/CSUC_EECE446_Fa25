8a9198a9361b414ea6a15f302d13ae980d716736
mycatdaphne <152930735+mycatdaphne@users.noreply.github.com>
2025-10-12 16:59:02 -0700

    program 2

---
+++ Program 2/program2.cpp
@@ -0,0 +1,135 @@
+#include <cstring>
+#include <cstdlib>
+#include <unistd.h>
+#include <iostream>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <vector>
+
+using namespace std;
+
+int reg_connect(const char* host, const char* port);
+void join(int sock, uint32_t peer_id);
+void search(int sock);
+void publish(int sock);
+
+int main(int argc, char *argv[]) {
+	if (argc != 4) {
+		cerr << "" << endl;
+		return 1;
+	}
+
+	const char *host = argv[1];
+	const char *port = argv[2];
+	uint32_t peer_id = atoi(argv[3]);
+
+	int sock = reg_connect(host, port);
+	if (sock < 0) {
+		cerr << "Could not connect to registry" << endl;
+		return 1;
+	}
+
+	string input;
+	while (true) {
+		cout << "Enter a command: ";
+		if (!(cin >> input)) break;
+		else if (input == "SEARCH") search(sock);
+		else if (input == "JOIN") join(sock, peer_id);
+		else if (input == "PUBLISH") publish(sock);
+		else if (input == "EXIT") break;
+		else cout << "Unknown command" << endl;
+	}
+
+	close(sock);
+	return 0;
+}
+
+int reg_connect(const char *host, const char *port) {
+	int sock;
+	struct sockaddr_in reg = {};
+	int port_num = atoi(port);
+
+	sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (sock < 0) {
+		perror("socket");
+		return -1;
+	}
+
+	reg.sin_family = AF_INET;
+	reg.sin_port   = htons(port_num);
+
+	if (inet_pton(AF_INET, host, &reg.sin_addr) <= 0) {
+		cerr << "Invalid address" << endl;
+		close(sock);
+		return -1;
+	}
+
+	if (::connect(sock, (struct sockaddr*)&reg, sizeof(reg)) < 0) {
+		perror("connect");
+		close(sock);
+		return -1;
+	}
+
+	return sock;
+
+}
+
+void join(int sock, uint32_t peer_id) {
+	uint8_t action = 0;
+	uint32_t id_net = htonl(peer_id);
+
+	char msg[5];
+	msg[0] = action;
+
+	memcpy(msg + 1, &id_net, 4);
+
+	send (sock, msg, 5, 0);
+}
+
+void search(int sock) {
+	uint8_t action = 2;
+	string filename;
+	cout << "Enter file name " << endl;
+	cin>> filename;
+
+	vector<unsigned char> msg;
+	msg.push_back(action);
+	for (uint32_t i = 0; i < filename.length(); i++) {
+		msg.push_back((unsigned char)filename[i]);
+	}
+	msg.push_back('\0');
+
+	send(sock, msg.data(), msg.size(), 0);
+
+	char buf[10];
+	int n = recv(sock, buf, 10, 0);
+	if (n <= 0) {
+		cerr << "Error receiving response" << endl;
+		return;
+	}
+
+	uint32_t peer_id, ip_raw;
+	uint16_t port;
+	memcpy(&peer_id, buf, 4);
+	memcpy(&ip_raw, buf + 4, 4);
+	memcpy(&port, buf + 8, 2);
+
+	peer_id = ntohl(peer_id);
+	ip_raw = ntohl(ip_raw);
+	port = ntohs(port);
+
+	if (peer_id == 0 && ip_raw == 0 && port == 0) {
+		cout << "File not indexed by registry" << endl;
+		return;
+	}
+
+	struct in_addr address;
+	address.s_addr = ip_raw;
+	char ip[INET_ADDRSTRLEN];
+	inet_ntop(AF_INET, &address, ip, sizeof(ip));
+
+	cout << "File found at" << endl;
+	cout << "Peer " << peer_id << endl;
+	cout << ip << ":" << port << endl;
+}
